4daefebc48714ce63fb403f78a0266d6e43276fe
Lefrec <blackfox244@gmail.com>
2026-01-09 14:28:45 +0100

    testing tools

--- src/pages/api/testLabAI.json.ts
+++ src/pages/api/testLabAI.json.ts
@@ -4,12 +4,6 @@ import weaviate, { type WeaviateClient } from 'weaviate-client';
 //get API key and URL
 const API_KEY = import.meta.env.LABAI_API_KEY;
 const API_URL = "https://lab-ia.umlp.fr/api/chat/completions";
-if (!API_KEY) {
-  console.warn("LABAI_API_KEY is not set");
-}
-if (!API_URL) {
-    console.warn("API URL not set");
-}
 
 //connect to local Weaviate DB
 const DBclient: WeaviateClient = await weaviate.connectToLocal();
@@ -34,64 +28,64 @@ export const POST: APIRoute = async ({ request }) => {
 
 //_____________________________________________________________________
 
-    try {
-      //QUERY TRANSFORMATION
-      //we give the last few messages to the llm and tell it to rewrite them into a proper search query for the DB
-      const recentMessages = messages.slice(-6);
-      //removing the first messages if they have the role assistant or system
-      while (recentMessages[0].role == "assistant" || recentMessages[0].role == "system") {
-        recentMessages.splice(0,1);
-      }
-      //creating the messages array for query transformation
-      const rewriteMessages: { role: string; content: string }[] = [
-        {
-          role: "system",
-          content: "Ton rôle est de réécrire le dernier message de l'utilisateur en une requête simple, courte et claire, destinée à une recherche dans une base de données vectorielle. La requête doit fonctionner seule et contenir les noms des personnages, lieux ou évènements concernés. Retourne uniquement la requête."
-        },
-        ...recentMessages,
-      ]
+//     try {
+//       //QUERY TRANSFORMATION
+//       //we give the last few messages to the llm and tell it to rewrite them into a proper search query for the DB
+//       const recentMessages = messages.slice(-6);
+//       //removing the first messages if they have the role assistant or system
+//       while (recentMessages[0].role == "assistant" || recentMessages[0].role == "system") {
+//         recentMessages.splice(0,1);
+//       }
+//       //creating the messages array for query transformation
+//       const rewriteMessages: { role: string; content: string }[] = [
+//         {
+//           role: "system",
+//           content: "Ton rôle est de réécrire le dernier message de l'utilisateur en une requête de recherche simple, courte et claire, pour une base de données vectorielle. La requête doit fonctionner seule et contenir les noms des personnages, lieux ou évènements concernés. Son but est de trouver les éléments les plus cohérents pour alimenter les réponses. Retourne uniquement la requête."
+//         },
+//         ...recentMessages,
+//       ]
 
-      //get the response from the llm
-      const rewriteRes = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${API_KEY}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-        model: "mistralai/Mistral-Small-3.2-24B-Instruct-2506",
-        messages : rewriteMessages,
-        }),
-      });
+//       //get the response from the llm
+//       const rewriteRes = await fetch(API_URL, {
+//         method: "POST",
+//         headers: {
+//           Authorization: `Bearer ${API_KEY}`,
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//         model: "mistralai/Mistral-Small-3.2-24B-Instruct-2506",
+//         messages : rewriteMessages,
+//         }),
+//       });
 
-      const data = await rewriteRes.json();
-      const rewrittenQuery = data.choices?.[0]?.message.content;
-      console.log("successfully rewritten query : " + rewrittenQuery);
+//       const data = await rewriteRes.json();
+//       const rewrittenQuery = data.choices?.[0]?.message.content;
+//       console.log("successfully rewritten query : " + rewrittenQuery);
     
-//_____________________________________________________________________
+// //_____________________________________________________________________
 
-      //RAG
-      //we search for relevant elements in the database and add them to the messages for better results    
-      const lore = DBclient.collections.use("Lore");
+//       //RAG
+//       //we search for relevant elements in the database and add them to the messages for better results    
+//       const lore = DBclient.collections.use("Lore");
     
-      const result = await lore.query.nearText(JSON.stringify(rewrittenQuery), {limit: 5});
+//       const result = await lore.query.nearText(JSON.stringify(rewrittenQuery), {limit: 5});
     
-      const systemMessage = {role: "system", content: "Tu peux t'aider des informations fournies pour répondre : "};
+//       const systemMessage = {role: "system", content: "Tu peux t'aider des informations fournies pour répondre : "};
     
-      for (let object of result.objects) {
-          systemMessage.content += (JSON.stringify(object.properties))+" | ";
-      };
-      console.log(systemMessage);
+//       for (let object of result.objects) {
+//           systemMessage.content += (JSON.stringify(object.properties))+" | ";
+//       };
+//       console.log(systemMessage);
     
-      //add the system message to the messages
-      messages = [
-          systemMessage,
-          ...body.messages,
-      ];
-      console.log("Query transformation and RAG succeded")
-    } catch (err) {
-      console.log("Query transformation and RAG search failed : "+err)
-    }
+//       //add the system message to the messages
+//       messages = [
+//           systemMessage,
+//           ...body.messages,
+//       ];
+//       console.log("Query transformation and RAG succeded")
+//     } catch (err) {
+//       console.log("Query transformation and RAG search failed : "+err)
+//     }
 
 //_____________________________________________________________________
 
